d7c7a3db6388b56ccdeedc6d4c5df6a46fc87fdd
Pedro Algarvio <pedro@algarvio.me>
2024-07-11 11:38:29 +0100

    Minor update to the `/` endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
-import cors from 'cors';
-import 'dotenv/config';
-import express from 'express';
-import { router } from './router';
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import { Request, Response } from "express";
+import { router } from "./router";
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
-app.get('/', (req, res) => res.send('Server is running!'));
-app.use('/api', router);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running! The api is getting served from '/api'");
+});
+app.use("/api", router);
 
 app.listen(port, () => {
- console.log(`App listening on port: ${port}`);
+  console.log(`App listening on port: ${port}`);
 });
